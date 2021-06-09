f8abe50025742118ee0430e259d3d9d877e34559
ZhengjunHUO <firelouiszj@hotmail.com>
2021-06-09 09:43:24 +0200

    solve 0445(medium)

---
+++ 0445_Add_Two_Numbers_II/naive.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func newList(l []int) *ListNode {
+	head := &ListNode{l[0], nil}
+	n := len(l)
+
+	if n == 1 {
+		return head
+	}
+
+	curr := head
+	for i:=1; i<n; i++ {
+		curr.Next = &ListNode{l[i], nil}
+		curr = curr.Next
+	}
+	return head
+}
+
+func printList(node *ListNode) {
+	rslt := []int{}
+	curr := node
+	for {
+		rslt = append(rslt, curr.Val)
+
+		if curr.Next == nil {
+			break
+		}
+
+		curr = curr.Next	
+	}
+
+	fmt.Println(rslt)
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	c1, c2 := l1, l2
+	s1, s2 := godtype.NewStack(), godtype.NewStack()
+
+	var curr *ListNode 
+	v1, v2, carry := 0, 0, 0
+
+	for c1 != nil {
+		s1.Push(c1.Val)
+		c1 = c1.Next	
+	}
+	for c2 != nil {
+		s2.Push(c2.Val)
+		c2 = c2.Next
+	}
+
+	for !s1.IsEmpty() || !s2.IsEmpty() || carry > 0 {
+		if !s1.IsEmpty() {
+			v1 = s1.Pop().(int)
+		}else{
+			v1 = 0
+		}
+		if !s2.IsEmpty() {
+			v2 = s2.Pop().(int)
+		}else{
+			v2 = 0
+		}
+
+		sum := v1 + v2 + carry
+		carry = sum/10
+
+		// 反向制造链表
+		prev := &ListNode{sum%10, curr}
+		curr = prev
+	}
+
+	return curr
+}
+
+func main(){
+	l1 := newList([]int{7,2,4,3})
+	l2 := newList([]int{5,6,4})
+	printList(addTwoNumbers(l1, l2))
+
+	l1 = newList([]int{2,4,3})
+	l2 = newList([]int{5,6,4})
+	printList(addTwoNumbers(l1, l2))
+
+	l1 = newList([]int{0})
+	l2 = newList([]int{0})
+	printList(addTwoNumbers(l1, l2))
+}
