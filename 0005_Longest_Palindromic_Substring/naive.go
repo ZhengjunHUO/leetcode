dd0d0bd0963a9080658332dd24a36a2df49d6cd8
ZhengjunHUO <firelouiszj@hotmail.com>
2021-06-17 13:19:06 +0200

    place holder for 0005

---
+++ 0005_Longest_Palindromic_Substring/naive.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func longestPalindrome(s string) string {
+    
+}
+
+func main() {
+	fmt.Println(longestPalindrome("babad"))
+	fmt.Println(longestPalindrome("cbbd"))
+	fmt.Println(longestPalindrome("a"))
+	fmt.Println(longestPalindrome("ac"))
+
+}
