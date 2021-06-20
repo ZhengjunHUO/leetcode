de19b2d5da76e8d9d70d705a8bfc10d316d06f19
ZhengjunHUO <firelouiszj@hotmail.com>
2021-06-20 11:39:53 +0200

    update to use outsourced kmp algo

---
+++ Tools/getKmpTable.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/gtoolkit"
+)
+
+func main() {
+	fmt.Println("Should be: [0, 1, 2, 3]")
+	fmt.Println(gtoolkit.GetKmpTable("AAAA"))
+	fmt.Println()
+	
+	fmt.Println("Should be: [0, 0, 0, 0, 0]")
+	fmt.Println(gtoolkit.GetKmpTable("ABCDE"))
+	fmt.Println()
+
+	fmt.Println("Should be: [0, 1, 0, 1, 2, 0, 1, 2, 3, 4, 5]")
+	fmt.Println(gtoolkit.GetKmpTable("AABAACAABAA"))
+	fmt.Println()
+
+	fmt.Println("Should be: [0, 1, 2, 0, 1, 2, 3, 3, 3, 4]")
+	fmt.Println(gtoolkit.GetKmpTable("AAACAAAAAC"))
+	fmt.Println()
+
+	fmt.Println("Should be: [0, 1, 2, 0, 1, 2, 3]")
+	fmt.Println(gtoolkit.GetKmpTable("AAABAAA"))
+	fmt.Println()
+
+	fmt.Println("Should be: [0, 0, 0, 1, 0, 0] ")
+	fmt.Println(gtoolkit.GetKmpTable("为我我为人人"))
+}
