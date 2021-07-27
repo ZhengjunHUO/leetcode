90f4e471aade6fc87cd935d2882753f6eedaa806
ZhengjunHUO <firelouiszj@hotmail.com>
2021-07-27 07:40:50 +0200

    solve 0506(easy)

---
+++ 0506_Relative_Ranks/naive.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func findRelativeRanks(score []int) []string {
+	n := len(score)
+	rslt := make([]string, n) 
+
+	pq := godtype.NewPQ([]int{}, []int{}, true)
+	// 分数作为priority值，内容为该元素的index
+	for i,v := range score{
+		pq.Push(i, v)
+	}
+
+	for pq.Peek() != nil {
+		idx := pq.Pop().(int)
+		switch n {
+			case 1:
+				rslt[idx] = "Gold Medal"
+			case 2:
+				rslt[idx] = "Silver Medal"
+			case 3:
+				rslt[idx] = "Bronze Medal"
+			default:
+				rslt[idx] = fmt.Sprint(n)
+		}		
+		n--
+	}
+
+	return rslt
+}
+
+func main() {
+	scores := [][]int{[]int{5,4,3,2,1}, []int{10,3,8,9,4}}
+	for _, v := range scores {
+		fmt.Println(findRelativeRanks(v))
+	}
+}
