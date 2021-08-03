f19efd698ed9375da09a6f97d61260e79a734042
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-03 09:32:22 +0200

    solve 0070(easy)

---
+++ 0070_Climbing_Stairs/naive.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func backtrack(n, curr int, cnt *int) {
+	if curr == n {
+		*cnt += 1
+		return
+	}
+
+	if curr < n {
+		backtrack(n, curr+1, cnt)
+	}
+	if curr < n-1 {
+		backtrack(n, curr+2, cnt)
+	}
+}
+
+func climbStairs(n int) int {
+	cnt := 0
+	backtrack(n, 0, &cnt)
+	return cnt    
+}
+
+func main() {
+	ns := []int{2,3,5}
+	for _,v := range ns {
+		fmt.Println(climbStairs(v))
+	}
+}
