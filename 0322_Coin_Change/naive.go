bf9a9479ba403933385c12a04e5064e459a3a240
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-06 08:39:31 +0200

    solve 0322(medium)

---
+++ 0322_Coin_Change/naive.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	// 最小面额为1，故最大值不会超过amount/1
+	for i := range dp {
+		dp[i] = amount+1
+	}
+	// 确保dp[0]为0
+	dp[0] = 0
+
+	for i:=0; i<=amount; i++ {
+		for _,c := range coins {
+			if i - c < 0 {
+				continue
+			}
+
+			if temp := dp[i-c] + 1; temp < dp[i] {
+				dp[i] = temp
+			}
+		} 
+	}
+
+	if dp[len(dp)-1] == amount+1 {
+		return -1	
+	}else{
+		return dp[len(dp)-1]
+	}
+}
+
+func main() {
+	coins := [][]int{[]int{1,2,5}, []int{2}, []int{1}, []int{1}, []int{1}}
+	amounts := []int{11,3,0,1,2}
+
+	for i := range coins {
+		fmt.Println(coinChange(coins[i], amounts[i]))
+	}
+}
