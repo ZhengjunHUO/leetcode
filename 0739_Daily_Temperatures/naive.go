da6eb09e251c15d3b1e36e74118be5ea34dfb864
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-06 08:36:03 +0200

    solve 0739(medium)

---
+++ 0739_Daily_Temperatures/naive.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func dailyTemperatures(temperatures []int) []int {
+	mono := godtype.NewStack()
+	rslt := make([]int, len(temperatures))
+	
+	for i := len(temperatures) - 1; i >= 0; i-- {
+		for !mono.IsEmpty() && temperatures[i] >= temperatures[mono.Peek().(int)] {
+			mono.Pop()
+		}
+
+		if !mono.IsEmpty() {
+			rslt[i] = mono.Peek().(int) - i
+		}
+
+		mono.Push(i)
+	}
+
+	return rslt
+}
+
+func main() {
+	temps := [][]int{[]int{73,74,75,71,69,72,76,73}, []int{30,40,50,60}}
+	for i := range temps {
+		fmt.Println(dailyTemperatures(temps[i]))
+	}
+}
