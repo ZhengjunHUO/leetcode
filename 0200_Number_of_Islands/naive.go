324b4029a0027450ea9a09fff95ef5c6b418fec1
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-16 07:32:17 +0200

    solve 0200(medium)

---
+++ 0200_Number_of_Islands/naive.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func numIslands(grid [][]byte) int {
+	m, n := len(grid), len(grid[0])
+	uf := godtype.NewUF(m*n)
+	directions := [][]int{[]int{0,0}, []int{1,0}, []int{1,1}, []int{0,1}}
+	
+	// 使用一个2*2的框来遍历矩阵
+	for i := 0; i < m - 1; i++ {
+		for j := 0; j < n - 1; j++ {
+			// 以左下右上的顺序遍历框中的四个元素，如果当前元素和下一个元素都是1则调用Union方法
+			for k := 0; k < 4; k ++ {
+				if grid[i+directions[k][0]][j+directions[k][1]] == '1' {
+					if grid[i+directions[(k+1)%4][0]][j+directions[(k+1)%4][1]] == '1' {
+						uf.Union((i+directions[k][0])*n+j+directions[k][1], (i+directions[(k+1)%4][0])*n+j+directions[(k+1)%4][1])
+					}
+				}
+			}
+		}
+	}
+
+	dict := make(map[int]int)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == '1' {
+				// 同一个岛的1指向同一个根结点
+				dict[uf.FindRoot(i*n+j)] += 1
+			}	
+		}
+	}
+	
+//	fmt.Println(dict)
+	return len(dict)
+}
+
+func main() {
+	grids := [][][]byte{[][]byte{[]byte{'1','1','1','1','0'}, []byte{'1','1','0','1','0'}, []byte{'1','1','0','0','0'}, []byte{'0','0','0','0','0'}}, [][]byte{[]byte{'1','1','0','0','0'}, []byte{'1','1','0','0','0'}, []byte{'0','0','1','0','0'}, []byte{'0','0','0','1','1'}}}
+	for i := range grids {
+		fmt.Println(numIslands(grids[i]))
+	}
+}
