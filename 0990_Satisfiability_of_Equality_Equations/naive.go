53cd5ad82a4f03c8a8e00fc55dfa43333473905d
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-16 07:31:36 +0200

    solve 0990(medium)

---
+++ 0990_Satisfiability_of_Equality_Equations/naive.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func equationsPossible(equations []string) bool {
+	uf := godtype.NewUF(26)
+
+	for i := range equations {
+		if equations[i][1] == '=' {
+			uf.Union(int(equations[i][0] - 'a'), int(equations[i][3] - 'a'))
+		}
+	}
+
+	for i := range equations {
+		if equations[i][1] == '!' {
+			if uf.IsLinked(int(equations[i][0] - 'a'), int(equations[i][3] - 'a')) {
+				return false
+			}
+		}
+	}
+    
+	return true
+}
+
+func main() {
+	eqs := [][]string{[]string{"a==b","b!=a"}, []string{"b==a","a==b"}, []string{"a==b","b==c","a==c"}, []string{"a==b","b!=c","c==a"}, []string{"c==c","b==d","x!=z"}}
+	for i := range eqs {
+		fmt.Println(equationsPossible(eqs[i]))
+	}
+}
