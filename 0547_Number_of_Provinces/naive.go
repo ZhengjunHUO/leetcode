e392615e597be197908e7296066d38b662b373d8
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-20 07:22:35 +0200

    solve 0547(medium)

---
+++ 0547_Number_of_Provinces/naive.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func findCircleNum(isConnected [][]int) int {
+	n := len(isConnected)
+	uf := godtype.NewUF(n) 
+
+	for i := 0; i < n - 1 ; i++ {
+		for j := i + 1; j < n; j++ {
+			if isConnected[i][j] == 1 {
+				uf.Union(i, j)
+			}
+		}
+	}
+
+	return uf.Count()
+}
+
+func main() {
+	ms := [][][]int{[][]int{[]int{1,1,0},[]int{1,1,0},[]int{0,0,1}}, [][]int{[]int{1,0,0},[]int{0,1,0},[]int{0,0,1}}}
+	for i := range ms {
+		fmt.Println(findCircleNum(ms[i]))
+	}
+}
