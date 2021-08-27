dc4c10ba245652929b8f6c14764057cddb240e5c
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-27 06:48:25 +0200

    solve 0530(easy)

---
+++ 0530_Minimum_Absolute_Difference_in_BST/naive.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func search(curr *godtype.TreeNode, min, max int) int {
+	if curr == nil {
+		return 100001
+	}
+
+	val, rslt := curr.Val.(int), 100001
+
+	if min != -1 {
+		rslt = min2(rslt, val-min)
+	}
+	if max != 100001 {
+		rslt = min2(rslt, max-val)
+	}
+
+	rslt = min2(rslt, search(curr.Left, min, val))
+	rslt = min2(rslt, search(curr.Right, val, max))
+	
+	return rslt
+}
+
+func getMinimumDifference(root *godtype.TreeNode) int {
+	return search(root, -1, 100001)
+}
+
+func main() {
+	trees := [][]interface{}{[]interface{}{4,2,6,1,3}, []interface{}{1,0,48,nil,nil,12,49}, []interface{}{12,1,36,nil,nil,14,49}}
+	for i := range trees {
+		fmt.Println(getMinimumDifference(godtype.NewBTree(trees[i])))
+	}
+}
