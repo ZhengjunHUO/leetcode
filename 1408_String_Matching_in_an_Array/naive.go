997e2d81eec685de762615c2b6aa5bfc122d86ee
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-28 15:42:27 +0200

    solve 1408(easy)

---
+++ 1408_String_Matching_in_an_Array/naive.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func stringMatching(words []string) []string {
+	s := ""
+	rslt := []string{}
+
+	for i := range words {
+		s = s + words[i] + " "
+	}
+
+	for i := range words {
+		if len(godtype.NewPatternFinder(words[i]).FindIn(s)) >= 2 {
+			rslt = append(rslt, words[i])
+		}
+	}
+
+	return rslt
+}
+
+func main() {
+	words := [][]string{[]string{"mass","as","hero","superhero"}, []string{"leetcode","et","code"}, []string{"blue","green","bu"}}
+	for i := range words {
+		fmt.Println(stringMatching(words[i]))
+	}
+}
