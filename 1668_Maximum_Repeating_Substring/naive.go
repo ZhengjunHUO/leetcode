358a19a498baa4691cfcad665f340948713ac8e4
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-29 07:33:50 +0200

    solve 1668(easy)

---
+++ 1668_Maximum_Repeating_Substring/naive.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func maxRepeating(sequence string, word string) int {
+	rslt := godtype.NewPatternFinder(word).FindIn(sequence)
+	if len(rslt) == 0 {
+		return 0
+	}
+
+	max, curr := 1, 1
+
+	for i := 1; i < len(rslt); i++ {
+		if rslt[i] - rslt[i-1] == len(word) {
+			curr++
+			if curr > max {
+				max = curr
+			}
+		}else{
+			curr = 1
+		}
+	}
+
+	return max
+}
+
+func main() {
+	seqs := []string{"ababc", "ababc", "ababc", "dabckkabcabcabcdeabcabck"}
+	words := []string{"ab", "ba", "ac", "abc"}
+
+	for i := range seqs {
+		fmt.Println(maxRepeating(seqs[i], words[i]))
+	}
+}
