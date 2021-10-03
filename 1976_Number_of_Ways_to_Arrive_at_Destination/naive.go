33bd39cddb2730e3861df90cfbfc09bd70d1ab75
ZhengjunHUO <firelouiszj@hotmail.com>
2021-10-03 15:18:10 +0200

    solve 1976(medium)

---
+++ 1976_Number_of_Ways_to_Arrive_at_Destination/naive.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+const MAXINT = int(^uint(0) >> 1)
+
+func countPaths(n int, roads [][]int) int {
+	// 邻接表
+	adj := make(map[int][][2]int)
+	for i := range roads {
+		adj[roads[i][0]] = append(adj[roads[i][0]], [2]int{roads[i][1], roads[i][2]})
+		adj[roads[i][1]] = append(adj[roads[i][1]], [2]int{roads[i][0], roads[i][2]})
+	}
+
+	// costTo表，每个节点对应一个二元组，在花费的基础上增加一个路径计数器
+	costTo := make([][2]int, n)
+	for i := range costTo {
+		costTo[i] = [2]int{MAXINT, 0}
+	}
+	costTo[0] = [2]int{0,1}
+
+	// 优先队列
+	pq := godtype.NewPQ([]int{0}, []int{0}, true)
+	for pq.Size() > 0 {
+                curr := pq.PopWithPrio()
+                currNode, costToCurr := curr[0].(int), curr[1].(int)
+
+		if costToCurr > costTo[currNode][0] {
+			continue
+		}
+
+		if v, ok := adj[currNode]; ok {
+			for i := range v {
+				costToNext := v[i][1] + costToCurr
+				// 如果从起点经curr到next的花费和记录中相等，需要增加计数器的数字
+				if costToNext == costTo[v[i][0]][0] {
+					// 增加的量为当前节点在记录中的计数
+					costTo[v[i][0]][1] += costTo[currNode][1]
+					continue
+				}
+
+				// 找到一条捷径，更新记录中next的花费和计数器（当前节点在记录中的计数）
+				if costToNext < costTo[v[i][0]][0] {
+					costTo[v[i][0]][0], costTo[v[i][0]][1] = costToNext, costTo[currNode][1]
+					pq.Push(v[i][0], costToNext)
+				}
+			}
+		}
+	}
+
+	return costTo[n-1][1]
+}
+
+func main() {
+	ns := []int{7,2}
+	roads := [][][]int{[][]int{[]int{0,6,7},[]int{0,1,2},[]int{1,2,3},[]int{1,3,3},[]int{6,3,3},[]int{3,5,1},[]int{6,5,1},[]int{2,5,1},[]int{0,4,5},[]int{4,6,2}}, [][]int{[]int{1,0,10}}}
+	for i := range roads {
+		fmt.Println(countPaths(ns[i], roads[i]))
+	}
+}
