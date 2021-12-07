be490b6fd8b5386bdf65e3ae986e6da574d9c31f
ZhengjunHUO <firelouiszj@hotmail.com>
2021-12-07 12:53:34 +0100

    solve 0112(easy)

---
+++ 0112_Path_Sum/naive.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func backtrack(current *godtype.TreeNode, remain int, hasPath *bool) {
+	if current == nil {
+		if remain == 0 {
+			*hasPath = true
+		}
+		return
+	}
+
+	if *hasPath == true {
+		return
+	}
+
+	curVal := current.Val.(int)
+	backtrack(current.Left, remain - curVal, hasPath)
+	backtrack(current.Right, remain - curVal, hasPath)
+}
+
+func hasPathSum(root *godtype.TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	var hasPath bool = false
+	backtrack(root, targetSum, &hasPath)
+
+	return hasPath
+}
+
+func main() {
+	trees := [][]interface{}{[]interface{}{5,4,8,11,nil,13,4,7,2,nil,nil,nil,1}, []interface{}{1,2,3}, []interface{}{}}
+	targetSums := []int{22,5,0}
+	for i := range trees {
+		fmt.Println(hasPathSum(godtype.NewBTree(trees[i]), targetSums[i]))
+	}
+}
