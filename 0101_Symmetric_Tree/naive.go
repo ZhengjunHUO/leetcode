6dd9d017c68d4407f9c8083be0ecd153ca213c05
ZhengjunHUO <firelouiszj@hotmail.com>
2021-12-07 15:29:35 +0100

    solve 0101(easy)

---
+++ 0101_Symmetric_Tree/naive.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ZhengjunHUO/godtype"
+)
+
+func recurs(left, right *godtype.TreeNode) bool {
+	if ( left == nil || right == nil) {
+		return left == right
+	}
+
+	if left.Val.(int) != right.Val.(int) {
+		return false
+	}
+
+	return recurs(left.Left, right.Right) && recurs(left.Right, right.Left)
+}
+
+func isSymmetric(root *godtype.TreeNode) bool {
+	return root != nil && recurs(root.Left, root.Right)
+}
+
+func main() {
+	trees := [][]interface{}{[]interface{}{1,2,2,3,4,4,3}, []interface{}{1,2,2,nil,3,nil,3}}
+	for i := range trees {
+		fmt.Println(isSymmetric(godtype.NewBTree(trees[i])));
+	}
+}
