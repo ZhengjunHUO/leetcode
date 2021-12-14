dbe85a2359d1be3bfad416b26b0755f06c9d67a2
ZhengjunHUO <firelouiszj@hotmail.com>
2021-12-14 20:28:15 +0100

    add XorProduct O(1) solution

---
+++ extra/xorproduct.go
@@ -0,0 +1,39 @@
+package main
+
+/* 
+  do a XOR production from 0 we can observe a rule of cycle of 4 
+	x  Bin  XOR_Prod_from_0
+	0  0000 0000 => 0 [x]
+	1  0001 0001 => 1 [1]
+	2  0010 0011 => 3 [x+1]
+	3  0011 0000 => 0 [0]
+	4  0100 0100 => 4 [x]
+	5  0101 0001 => 1 [1]
+	6  0110 0111 => 7 [x+1]
+	7  0111 0000 => 0 [0]
+	8  1000 1000 => 8 [x]
+	9  1001 0001 => 1 [1]
+	10 1010 1011 => 11 [x+1]
+	11 1011 0000 => 0 [0]
+*/
+func prodFromZeroToX(x int) int {
+	prod := []int{x, 1, x+1, 0}
+	return prod[x%4]
+}
+
+/*
+   because:
+	wanted = M ^ (M+1) ^ ... ^ (N-1) ^ N
+	prodFromZeroToX(N) = (0 ^ 1 ^ ... ^ M-1) ^ (M ^ M+1 ^ ... ^ N-1 ^ N)
+			   = prodFromZeroToX(M-1) ^ wanted
+   then XOR prodFromZeroToX(M-1) in both side we have:
+	wanted = prodFromZeroToX(N) ^ prodFromZeroToX(M-1)
+*/
+func XorProduct(M int, N int) int {
+	if M == N {
+		return 0
+	}
+
+	return prodFromZeroToX(N) ^ prodFromZeroToX(M-1)
+}
+
---
+++ extra/xorproduct_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestXorProduct(t *testing.T) {
+	a, b, want := []int{5, 12}, []int{8, 21}, []int{12, 1}
+	for i := range a {
+		if temp := XorProduct(a[i], b[i]); temp != want[i] {
+			t.Errorf("XorProduct(%d, %d) return %d but want %d", a[i], b[i], temp, want[i])
+		}
+	}
+}
