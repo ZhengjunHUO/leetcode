c8c22995c20f590c40601d3794e3563c362b7687
ZhengjunHUO <firelouiszj@hotmail.com>
2022-03-13 21:50:50 +0100

    update comments

--- 2127_Maximum_Employees_to_Be_Invited_to_a_Meeting/topo.go
+++ 2127_Maximum_Employees_to_Be_Invited_to_a_Meeting/topo.go
@@ -13,7 +13,7 @@ func max(a, b int) int {
 	return b
 }
 
-// Learned from @heshan1234
+// 参考@heshan1234
 func maximumInvitations(favorite []int) int {
 	n := len(favorite)
 
@@ -22,11 +22,13 @@ func maximumInvitations(favorite []int) int {
 	q := godtype.NewQueue()
 
 	// calculate the "popularity" of each node
+	// 计算每一个节点的入度，即有几个箭头指向它
 	for i := range favorite {
 		ingr[favorite[i]]++
 	}
 
 	// find out the node no one "appreciate"
+	// 从入度为0的节点出发找无环链（加入一个队列并标记为已访问）
 	for i := range ingr {
 		if ingr[i] == 0 {
 			q.Push(i)
@@ -40,6 +42,7 @@ func maximumInvitations(favorite []int) int {
 		dst := favorite[src]
 		dp[dst] = max(dp[dst], dp[src]+1)
 
+		// 关键步骤，用来发现某些入度非0的节点其实是无环链的一部分
 		ingr[dst]-=1
 		if ingr[dst] == 0 {
 			q.Push(dst)
@@ -50,7 +53,9 @@ func maximumInvitations(favorite []int) int {
 	/* 
 	   - if loop's size is 2, we can attach an acyclic chain at each end (at most 2),
 	     and we can have serveral size==2 loops in result
+	     尺寸等于2的环可以包容其他大小为2的环
 	   - if loop's size > 2, we can only have at most one size>2 loop in result
+	     尺寸大于2的环有排它性
 	*/
 	loopOfTwo, loop := 0, 0
 	for i := range visited {
